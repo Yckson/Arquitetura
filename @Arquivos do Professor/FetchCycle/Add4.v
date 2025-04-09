8b9eea0a5d3c94cdb4ce6c3286915ccc6f690070
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2025-04-09 00:10:07 -0300

    Remove unused files @ArquivosProfessor

--- @Arquivos do Professor/FetchCycle/Add4.v
+++
@@ -1,6 +0,0 @@
-module Add4(
-    input wire [31:0] in,        // Endereço atual do PC
-    output wire [31:0] out       // Endereço incrementado
-);
-    assign out = in + 32'd4;     // Soma 4 ao valor de entrada
-endmodule
